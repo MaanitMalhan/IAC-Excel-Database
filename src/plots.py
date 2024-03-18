4b0c562bd2f2f490b3090eaff370258b9356b375
Maanit972 <maanitmalhan@gmail.com>
2024-03-18 15:10:32 -0400

    mostly done just need finish calculations and plotly implementation

--- src/plots.py
+++ src/plots.py
@@ -1,1 +1,13 @@
-#Use plotly for visualization: number of reccs per assessment
+import plotly
+import openpyxl
+
+
+def plot_creation():
+    pass
+
+def plotly_graphs_in_excel(workbook):
+    destination_workbook = workbook
+    destination_sheet = destination_workbook.create_sheet(title="Graphs")
+    graph_sheet = destination_workbook['Graphs']
+    
+    
